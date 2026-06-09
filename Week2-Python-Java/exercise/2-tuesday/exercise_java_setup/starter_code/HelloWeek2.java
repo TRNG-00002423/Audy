a761fb95a88970de8e4fa070079d59ac5b8b8c67
audy <veeaudy1201@gmail.com>
2026-06-09 13:26:53 -0400

    added exercise_java_setup

---
+++ Week2-Python-Java/exercise/2-tuesday/exercise_java_setup/starter_code/HelloWeek2.java
@@ -0,0 +1,22 @@
+// **  Greets user back with a response based on optional arguments given :
+// args[0] = first name
+// args[1] = last name
+// then prints the current java runtime version */
+
+public class HelloWeek2 {
+  public static void main(String[] args) {
+    
+    if (args.length >= 2){
+      System.out.println("Hello," + args[0] +  " " + args[1] + "!");
+    }
+    else if (args.length >= 1){
+      System.out.println("Hello" + args[0] + "!");
+    }
+    else {
+      System.out.println("Helle, trainee!");
+    }
+    System.out.println("Java Runtime Version: " + Runtime.version());
+ 
+  }
+  
+}
